02a3cf368a4bf626c4de05d4ef5fb4d21904b64f
Danny Gilberg <danny.gilberg@loreal.com>
2024-08-22 15:25:23 -0400

    adds db file and code_run file

--- fbref/scraping_script.py
+++ fbref/scraping_script.py
@@ -7,10 +7,11 @@ import urllib.request
 import ssl
 import itertools
 import json
+import itertools
 import sqlite3
 from datetime import datetime, date, timedelta
 # from google.oauth2 import service_account
-from pandas_gbq import to_gbq
+# from pandas_gbq import to_gbq
 from urllib.request import urlopen
 # from google.cloud import storage, bigquery
 
@@ -311,7 +312,9 @@ def upsert_df(df, table_name, db_config):
         '''
         cursor.execute(sql, tuple(row))
         
+        
     conn.commit()
+    conn.close()
     
     
 def build_competitions_df(info):
@@ -350,3 +353,71 @@ def build_team_schedules(schedule_df):
               'xg_for', 'xg_against', 'match_id', 'match_date', 'home_or_away']
     final = pd.DataFrame(df_rows, columns=cols)
     return final
+
+
+if __name__ == "__main__":
+    st = datetime.now()
+    
+    
+    ssl._create_default_https_context = ssl._create_unverified_context
+    
+    with open("data_config.yaml", 'r') as stream:
+        data_config = yaml.safe_load(stream)
+        
+    with open("db_config.yaml", 'r') as stream:
+        db_config = yaml.safe_load(stream)
+        
+    with open("scraping_config.yaml", 'r') as stream:
+        scraping_config = yaml.safe_load(stream)
+        
+    with open("scraping_config.yaml", 'r') as stream:
+        scraping_config = yaml.safe_load(stream)
+        
+    with open("config.yaml", 'r') as stream:
+        config = yaml.safe_load(stream)
+    
+    with open("leagues.yaml", 'r') as stream:
+        info = yaml.safe_load(stream)
+    
+    
+    league_name = 'Premier-League'
+    season = '2022-2023'
+    league = info[league_name]
+    
+    raw_schedule = scrape_schedule(league, season)
+    schedule = clean_schedule(raw_schedule, data_config, league, season)
+    
+    upsert_df(schedule, 'dim_matches', db_config)
+    upsert_df(schedule, 'dim_squads', db_config)
+    
+    teams = build_team_schedules(schedule)
+    upsert_df(teams, 'dim_team_matches', db_config)
+    
+    
+    #scrape_from_schedule(schedule, scraping_config)
+    
+    ids = list(schedule.id)
+    categories = ['summary', 'passing', 'possession']
+    combos = list(itertools.product(ids, categories))
+    for j in combos:
+        match_id = j[0]
+        cat = j[1]
+        raw_home_file = 'raw_data/match_reports/{}/home_team_{}_{}.pkl'.format(cat, cat, match_id)
+        raw_away_file = 'raw_data/match_reports/{}/away_team_{}_{}.pkl'.format(cat, cat, match_id)
+        clean_match_report(raw_home_file, cat, data_config)
+        clean_match_report(raw_away_file, cat, data_config)
+        home_file = 'data/match_reports/{}/home_team_{}_{}.pkl'.format(cat, cat, match_id)
+        away_file = 'data/match_reports/{}/away_team_{}_{}.pkl'.format(cat, cat, match_id)
+        home_df = pd.read_pickle(home_file)
+        away_df = pd.read_pickle(away_file)
+        if cat == 'summary':
+            upsert_df(home_df, 'dim_player_appearances', db_config)
+            upsert_df(away_df, 'dim_player_appearances', db_config)
+            upsert_df(home_df, 'dim_players', db_config)
+            upsert_df(away_df, 'dim_players', db_config)
+        table = 'f_player_match_{}'.format(cat)
+        idf = pd.concat([away_df, home_df], ignore_index=True)
+        upsert_df(idf, table, db_config)
+    
+    et = datetime.now()
+    print(et-st)
